17dd435a555b693ff1df6b01b96fb02ea81cf5ae
przemek milke <theblimek@gmail.com>
2024-03-10 11:15:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet2_7_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cześć za chwilę podasz trzy liczby a ja sprawdzę która jest liczbą największą");
+            Console.WriteLine("Podaj pierwszą liczbę");
+            int liczba1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Dziękuje...podaj drugą liczbę");
+            int liczba2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Dziękuje bardzo...podaj ostatnią liczbę");
+            int liczba3 = Int32.Parse(Console.ReadLine());
+
+            if(liczba1 > liczba2 && liczba1 > liczba3)
+            {
+                Console.WriteLine($"Liczba {liczba1} jest największą wpisaną liczbą");
+            }
+            else if (liczba2 > liczba1 && liczba2 > liczba3)
+            {
+                Console.WriteLine($"Liczba {liczba2} jest największą wpisaną liczbą");
+
+            }
+            else
+            {
+                Console.WriteLine($"liczba {liczba3}wpisana to liczba najwieksza");
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
